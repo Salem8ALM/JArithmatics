57a9c8e62957dca439ef5d47070d4200f207836d
SALEM .. <salemsnsalmutairi@gmail.com>
2024-11-03 11:10:13 +0300

    ARITHMATICS

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        double amountKWD = 86;
+        System.out.println((int)amountKWD+ "KWD = "+ (int)(amountKWD *3.25) +"USD");
+
+        //challange
+        double amountUSD = 99;
+        double amountEURO=77;
+        System.out.println((int)amountUSD+ "USD = "+ (amountUSD /3.25)+"KWD");
+        System.out.println((int)amountEURO+ "EURO = "+(amountEURO *0.33)+"KWD");
+        }
+    }
